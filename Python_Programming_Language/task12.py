c556b7eb8225c42d1376a7f0e8c7390053116884
festus84 <festusaakinyemi84@gmail.com>
2022-10-13 17:40:27 +0100

    add task12

---
+++ Python_Programming_Language/task12.py
@@ -0,0 +1,18 @@
+# Declare a global variable
+
+language = "Python"  # global variable
+
+
+# Create a function, in the function declare a variable with name as language
+# and value as "Java", then print out the variable in the function
+
+# Print out the variable name into the console
+
+# Invoke the function
+
+def program():
+    language = "Java"
+    print("language", language)
+
+
+program()
